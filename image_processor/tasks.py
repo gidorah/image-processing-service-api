6c9d8eae20291ecd4b9920dc61cf3154dc580a21
Onur Akyüz <onur_akyuz@icloud.com>
2025-03-28 13:32:38 +0300

    Refactor apply_transformations task to improve error handling and update task status management

--- image_processor/tasks.py
+++ image_processor/tasks.py
@@ -3,7 +3,7 @@ import logging
 from celery import shared_task
 from PIL import Image
 
-from api.models import SourceImage, TransformationTask, TransformedImage
+from api.models import SourceImage, TaskStatus, TransformationTask, TransformedImage
 
 logger = logging.getLogger(__name__)
 
@@ -20,22 +20,55 @@ def apply_transformations(task_id):
     communication costs.
     """
 
-    task: TransformationTask = TransformationTask.objects.get(pk=task_id)
+    try:
+        task: TransformationTask = TransformationTask.objects.get(pk=task_id)
 
-    if not task.transformations:
-        raise Exception("No transformations found")  # TODO: Add custom exception
+        if not task:
+            raise Exception("Task not found")  # TODO: Add custom exception
 
-    logger.info(f"Applying transformations for task {task.id}")
+        task.status = TaskStatus.IN_PROGRESS
+        task.save()
 
-    original_image = task.original_image
+        if not task.transformations:
+            raise Exception("No transformations found")  # TODO: Add custom exception
 
-    image_file = task.original_image.file
-    image = Image.open(image_file)
+        logger.info(f"Applying transformations for task {task.id}")
 
-    for transformation in task.transformations:
-        # TODO: Apply transformation
-        logger.info(f"Applying transformation {transformation}")
-        pass
+        original_image = task.original_image
+
+        if not original_image:
+            raise Exception("Original image not found")  # TODO: Add custom exception
+
+        image_file = original_image.file
+        image = Image.open(image_file)
+
+        for transformation_key, transformation in task.transformations.items():
+            logger.info(f"Applying transformation {transformation_key}")
+            TRANSFORMATION_MAP[transformation_key](image, **transformation)
+
+        # TODO: Add image assignment to the ImageField
+
+        result_image = TransformedImage(
+            owner=task.owner,
+            file=image,
+            file_name=original_image.file_name,
+            description=original_image.description,
+            metadata=original_image.metadata,
+            source_image=original_image,
+            transformation_task=task,
+        )
+
+        result_image.save()
+        task.result_image = result_image
+
+    except Exception as e:
+        logger.error(f"Error while applying transformations: {e}")
+        task.status = TaskStatus.FAILED
+        task.save()
+        raise e
+
+    task.status = TaskStatus.SUCCESS
+    task.save()
 
 
 def crop(image, x, y, width, height):
@@ -43,59 +76,65 @@ def crop(image, x, y, width, height):
     Crop an image.
     """
     box = (x, y, x + width, y + height)
-    transformed_image = image.crop(box)
-    return transformed_image
+    image = image.crop(box)
 
 
-def resize(image_file, width, height):
+def resize(image, width, height):
     """
     Resize an image.
     """
     pass
 
 
-def rotate(image_file, degrees):
+def rotate(image, degrees):
     """
     Rotate an image.
     """
     pass
 
 
-@shared_task
-def watermark(image_file, watermark_file):
+def watermark(image, watermark_file):
     """
     Watermark an image.
     """
     pass
 
 
-@shared_task
-def flip(image_file):
+def flip(image):
     """
     Flip an image.
     """
     pass
 
 
-@shared_task
-def mirror(image_file):
+def mirror(image):
     """
     Mirror an image.
     """
     pass
 
 
-@shared_task
-def change_format(image_file, format):
+def change_format(image, format):
     """
     Change the format of an image.
     """
     pass
 
 
-@shared_task
-def apply_filter(image_file, filter):
+def apply_filter(image, *args, **kwargs):
     """
     Apply a filter to an image.
     """
     pass
+
+
+TRANSFORMATION_MAP = {
+    "crop": crop,
+    "resize": resize,
+    "rotate": rotate,
+    "watermark": watermark,
+    "flip": flip,
+    "mirror": mirror,
+    "change_format": change_format,
+    "apply_filter": apply_filter,
+}

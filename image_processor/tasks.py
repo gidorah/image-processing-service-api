7b6a5625829e6a6d6b794cc97e6ffd9db45a0423
Onur Akyüz <onur_akyuz@icloud.com>
2025-03-28 21:51:30 +0300

    Refactor apply_transformations task to use custom exceptions and improve image processing logic

--- image_processor/tasks.py
+++ image_processor/tasks.py
@@ -1,9 +1,17 @@
+import io
 import logging
 
 from celery import shared_task
+from django.core.files import File
 from PIL import Image
 
+from api.exceptions import (
+    NoTransformationsDefined,
+    OriginalImageNotFound,
+    TaskNotFound,
+)
 from api.models import SourceImage, TaskStatus, TransformationTask, TransformedImage
+from utils.utils import extract_metadata
 
 logger = logging.getLogger(__name__)
 
@@ -24,41 +32,71 @@ def apply_transformations(task_id):
         task: TransformationTask = TransformationTask.objects.get(pk=task_id)
 
         if not task:
-            raise Exception("Task not found")  # TODO: Add custom exception
+            raise TaskNotFound()
 
         task.status = TaskStatus.IN_PROGRESS
         task.save()
 
         if not task.transformations:
-            raise Exception("No transformations found")  # TODO: Add custom exception
+            raise NoTransformationsDefined()
 
         logger.info(f"Applying transformations for task {task.id}")
 
         original_image = task.original_image
 
         if not original_image:
-            raise Exception("Original image not found")  # TODO: Add custom exception
+            raise OriginalImageNotFound()
 
         image_file = original_image.file
         image = Image.open(image_file)
 
+        image_format = original_image.metadata.get("format")
+
+        # If the format is not defined, use the original image format
+        # as the default format. When saving the image, the format
+        # will be changed to the format specified in the task.
+        # Also remove the "change_format" transformation from the task
+        # because it's not a transformation function.
+        if "change_format" in task.transformations:
+            image_format = task.transformations["change_format"].get("format")
+            task.transformations.pop("change_format")
+
         for transformation_key, transformation in task.transformations.items():
             logger.info(f"Applying transformation {transformation_key}")
-            TRANSFORMATION_MAP[transformation_key](image, **transformation)
+            image = TRANSFORMATION_MAP[transformation_key](image, **transformation)
 
-        # TODO: Add image assignment to the ImageField
+        # Ensure the PIL image is in RGB mode (if saving as JPEG)
+        if image.mode == "RGBA":
+            image = image.convert("RGB")
 
-        result_image = TransformedImage(
+        # Create a BytesIO buffer to temporarily store the image
+        image_buffer = io.BytesIO()
+
+        # Save the PIL image to the buffer with the specified format
+        image.save(image_buffer, format=image_format)
+
+        # Reset buffer position to the beginning
+        image_buffer.seek(0)
+
+        file_name = f"{original_image.file_name}_{task.id}.{image_format.lower()}"
+
+        # Create a Django File object from the buffer
+        django_file = File(image_buffer, name=file_name)
+
+        # Create and save the BaseImage instance
+        result_image = TransformedImage.objects.create(
             owner=task.owner,
-            file=image,
-            file_name=original_image.file_name,
+            file=django_file,
+            file_name=file_name,
             description=original_image.description,
-            metadata=original_image.metadata,
             source_image=original_image,
             transformation_task=task,
         )
 
         result_image.save()
+        # Close the buffer to free memory
+        image_buffer.close()
+
         task.result_image = result_image
 
     except Exception as e:
@@ -71,63 +109,64 @@ def apply_transformations(task_id):
     task.save()
 
 
-def crop(image, x, y, width, height):
+def crop(image: Image, x, y, width, height) -> Image:
     """
     Crop an image.
     """
     box = (x, y, x + width, y + height)
-    image = image.crop(box)
+    return image.crop(box)
 
 
-def resize(image, width, height):
+def resize(image: Image, width, height) -> Image:
     """
     Resize an image.
     """
-    pass
+    return image.resize((width, height))
 
 
-def rotate(image, degrees):
+def rotate(image: Image, degrees) -> Image:
     """
     Rotate an image.
     """
-    pass
+    return image.rotate(angle=degrees)
 
 
-def watermark(image, watermark_file):
+def watermark(image: Image, watermark_file) -> Image:
     """
     Watermark an image.
     """
-    pass
+    return image
 
 
-def flip(image):
+def flip(image: Image) -> Image:
     """
     Flip an image.
     """
     pass
 
 
-def mirror(image):
+def mirror(image: Image) -> Image:
     """
     Mirror an image.
     """
     pass
 
 
-def change_format(image, format):
-    """
-    Change the format of an image.
-    """
-    pass
-
-
-def apply_filter(image, *args, **kwargs):
+def apply_filter(image: Image, *args, **kwargs) -> Image:
     """
     Apply a filter to an image.
     """
-    pass
+    return image
 
 
+"""
+The key is the name of the transformation function.
+The value is the function itself.
+
+This dictionary won't include the "change_format" transformation
+because it's not a transformation function. And format change
+is done in the apply_transformations function.
+"""
 TRANSFORMATION_MAP = {
     "crop": crop,
     "resize": resize,
@@ -135,6 +174,5 @@ TRANSFORMATION_MAP = {
     "watermark": watermark,
     "flip": flip,
     "mirror": mirror,
-    "change_format": change_format,
     "apply_filter": apply_filter,
 }

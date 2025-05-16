641bf9ccb78e8c73bf2abf4692e012b7cfcfbab2
Onur Akyüz <onur_akyuz@icloud.com>
2025-05-16 22:13:28 +0300

    Remove duplicate metadata extraction for image format in extract_metadata function

--- utils/utils.py
+++ utils/utils.py
@@ -20,7 +20,6 @@ def extract_metadata(image) -> dict:
         metadata["mode"] = image.mode
         metadata["width"] = image.width
         metadata["height"] = image.height
-        metadata["format"] = image.format
         # metadata["size"] = image_file.size
     except Exception as e:
         raise MetadataExtractionFailed(message=str(e))
